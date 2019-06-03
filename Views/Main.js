61a23aee85212589ad8b643c3029d0dad36d90ed
Maxor4 <krl.meven@gmail.com>
2019-06-03 09:07:48 +0200

    test

--- Views/Main.js
+++ Views/Main.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+    AsyncStorage,
+    StyleSheet,
+    Text,
+    Vibration,
+    View,
+} from "react-native";
 import Couleurs from '../scripts/Couleurs';
+import WebService from '../scripts/WebService';
+
+import {ws} from '../index.js';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,6 +19,155 @@ const instructions = Platform.select({
 });
 
 export default class Main extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            recherche: false,
+            refreshing: false,
+            listeRecherche: [],
+            resultat: true,
+            derniers: true,
+            categorie : null,
+        };
+
+        this.props.navigator.setOnNavigatorEvent(this.DeepLinkEvent.bind(this));
+        this.chargerListe(false);
+    }
+
+    DeepLinkEvent(event) {
+        if (event.type === "DeepLink") {
+            const parts = event.link.split("/");
+            const payload = event.payload;
+
+            switch (parts[0]) {
+
+                case "rechercheProduit":
+                    this.recherche(payload);
+                    break;
+
+                case "retourRecherche" :
+                    this.setState({
+                        recherche: false,
+                        resultat: true,
+                        derniers: true,
+                    });
+                    this.chargerListe();
+                    break;
+
+                case "recherche" :
+                    if (!this.state.recherche) {
+                        this.setState({
+                            recherche: true,
+                            derniers: true,
+                        });
+                    }
+                    break;
+
+                case "clearCache" :
+                    this.setState({
+                        listeRecherche: [],
+                    });
+                    break;
+            }
+        } else {
+            switch (event.id){
+                case "didAppear":
+                    this.chargerListe();
+                    this.setState({
+                        recherche: false,
+                    });
+                    break;
+                case "willDisappear":
+                    this.setState({
+                        recherche: false,
+                    });
+                    break;
+            }
+        }
+    }
+
+    recherche(text) {
+        let present = false;
+
+        if (typeof text === "undefined") {
+            this.chargerListe();
+            this.setState({
+                refreshing: false,
+                resultat: true,
+                derniers: true,
+            });
+        } else {
+            this.setState({refreshing: true}, () => {
+
+                AsyncStorage.getItem("listeProduits", (err, rechercheValue) => {
+                    const rechercheCache = JSON.parse(rechercheValue);
+
+                    if (text !== null && text.length > 0) {
+                        const productFilter = {},
+                            regex = text.toLowerCase();
+
+                        if (rechercheCache !== null) {
+                            Object.keys(rechercheCache).forEach((key) => {
+                                if (rechercheCache[key].libelle.toLowerCase().includes(regex) || rechercheCache[key].reference.toLowerCase().includes(regex)) {
+                                    productFilter[key] = rechercheCache[key];
+                                    present = true;
+                                }
+                            });
+                        }
+
+                        ws.rechercheProduit(text, (data) => {
+                            if (data.produits.length > 0) {
+                                present = true;
+                                data.produits.forEach((produit) => {
+                                    productFilter[produit.id_product+"-"+produit.id_product_attribute] = produit;
+                                });
+                            }
+
+                            this.setState({
+                                refreshing: false,
+                                resultat: present,
+                                derniers: !present,
+                                listeRecherche: present ?
+                                    productFilter :
+                                    []
+                            });
+                        }, () => {
+                            this.setState({
+                                refreshing: false,
+                                resultat: present,
+                                derniers: !present,
+                            });
+                        });
+                    } else {
+                        this.setState({
+                            refreshing: false,
+                            resultat: present,
+                            derniers: !present,
+                        });
+                    }
+                });
+            });
+        }
+    }
+
+    chargerListe(categories) {
+        if(categories){
+            ws.chargerTout((data) => {
+                console.log(data);
+                this.setState({
+                    listeRecherche: data
+                });
+            })
+        } else {
+            ws.getMots(this.state.categorie, (liste) =>{
+                this.setState({
+                    listeRecherche: liste
+                });
+            })
+        }
+    }
 
     render() {
         return (
